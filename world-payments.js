1b02e9ad161bbef771cf77372e7ee4938861dcbc
root <root@tmpl-debian-10.1>
2022-06-23 09:45:56 +0300

    add new settings

--- world-payments.js
+++ world-payments.js
@@ -17,16 +17,16 @@ export let options = {
 
     // Можно раскомментировать этот фрагмент, чтобы бить мимо CF прямо в Nginx.
     /*hosts: {
-        'world.fbk.info:443': '35.187.32.208:443',
+        'survey.navalnyteam.com:443': '51.38.35.219:443',
     },*/
 };
 
 export default function() {
     // Передаём случайные параметры URL-запроса, чтобы нейтрализовать CloudFlare.
     // Если nginx-ingress будет игнорировать неизвестные параметры URL, то он сможет закешировать ответ.
-    let response = http.get("https://world.fbk.info/?" + randomString(3) + '=' + randomString(10), {
+    let response = http.get("https://survey.navalnyteam.com/?" + randomString(3) + '=' + randomString(10), {
         headers: {
-            // world.fbk.info возвращает заголовок "Vary: Accept-Encoding". Это значит, что
+            // web- front возвращает заголовок "Vary: Accept-Encoding". Это значит, что
             // если клиент отправляет другой Accept-Encoding, это будет кеш-промах, и запрос
             // пройдёт в бэкенд
             'Accept-Encoding': randomString(5),
